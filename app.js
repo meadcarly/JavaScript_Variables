ff785ebf95486b33add71abb7c726eaf83c58de3
Carly Mead <meadcarly16@gmail.com>
2024-08-15 10:02:23 -0600

    Completed declaring and initializing variables in JavaScript

--- app.js
+++ app.js
@@ -4,7 +4,42 @@ console.log(
 );
 
 // Exercise 1
+let lastName;
+var age;
 
 // Exercise 2
+const firstName = "Carly";
+lastName = "Mead";
+age = 33;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+/*Declare a variable named price using the let keyword, and assign it the value 19.99 (number)
+Declare a variable named isOnSale using the let keyword, and assign it the value false (boolean)
+Declare a variable named salePercentage using the let keyword, and assign it the value 15 (number)
+Declare a variable named stock using the let keyword, and assign it the value 0 (number)
+Declare a variable named inStock using the let keyword, and assign it the value false (boolean)
+Declare a variable named selectedSize using the let keyword, and assign it the value "M" (string)*/
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+/*Declare a variable named title using the let keyword, and assign it the value "Name of the Wind" (string)
+Declare a variable named author using the let keyword, and assign it the value "Patrick Rothfuss" (string)
+Declare a variable named pageCount using the let keyword, and assign it the value 722 (number)
+Declare a variable named bookmark using the let keyword, and assign it the value 456 (number)
+Declare a variable named hasRead using the let keyword, and assign it the value true (boolean)
+ */
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
